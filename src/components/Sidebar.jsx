8fa2f8353eb525ee74bcd072a032762777982bef
Het Kanjariya <kanjariyahet24@gmail.com>
2026-03-29 13:12:17 +0530

    Reupdated UI

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -5,23 +5,23 @@ export default function Sidebar({open, setOpen}) {
     <>
       {/* Sidebar */}
       <div
-        className={`fixed top-0 left-0 z-50 transform transition-transform duration-300 bg-[#243B55] h-110 w-1/2 text-white rounded-r-xl backdrop-blur-md bg-[#243B55]/70
+        className={`fixed top-0 left-0 z-50 transform transition-transform duration-300 bg-indigo-900/50 h-110 w-1/2 text-white rounded-r-xl backdrop-blur-md bg-indigo-900/50
         ${open ? "translate-x-0" : "-translate-x-full"}`}
       >
         {/* Close button */}
         <button
           onClick={() => setOpen(false)}
-          className="text-2xl ml-4 mt-4 text-[oklch(57.7%_0.245_27.325)]"
+          className="text-2xl ml-4 mt-4 text-white"
         >
           <FaTimes />
         </button>
 
-        <div className="flex flex-col gap-4 text-center items-center mt-16 gap-5 text-xl font-semibold underline ">
-        <a href="#home" className="px-4 py-2 rounded-md hover:bg-sky-500" onClick={() => setOpen(false)}>Home</a>
-        <a href="#about" className="px-4 py-2 rounded-md hover:bg-sky-500" onClick={() => setOpen(false)}>About</a>
-        <a href="#services" className="px-4 py-2 rounded-md hover:bg-sky-500" onClick={() => setOpen(false)}>Services</a>
-        <a href="#projects" className="px-4 py-2 rounded-md hover:bg-sky-500" onClick={() => setOpen(false)}>Projects</a>
-        <a href="#contact" className="px-4 py-2 rounded-md hover:bg-sky-500" onClick={() => setOpen(false)}>Contact</a>
+        <div className="flex flex-col gap-4 text-center items-center mt-16 gap-8 text-xl font-semibold underline ">
+        <a href="#home" className="text-slate-200 hover:text-sky-400 transition duration-300" onClick={() => setOpen(false)}>Home</a>
+        <a href="#about" className="text-slate-200 hover:text-sky-400 transition duration-300" onClick={() => setOpen(false)}>About</a>
+        <a href="#services" className="text-slate-200 hover:text-sky-400 transition duration-300" onClick={() => setOpen(false)}>Services</a>
+        <a href="#projects" className="text-slate-200 hover:text-sky-400 transition duration-300" onClick={() => setOpen(false)}>Projects</a>
+        <a href="#contact" className="text-slate-200 hover:text-sky-400 transition duration-300" onClick={() => setOpen(false)}>Contact</a>
         </div>
       </div>
     </>
